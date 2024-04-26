b8e7b1799c97f93a39056fe26e4833999866dded
tkyen <tkyen1110@gmail.com>
2024-04-26 15:28:48 +0800

    Finish HW3/Problem3.c

--- HW3/Problem3.c
+++ HW3/Problem3.c
@@ -42,18 +42,33 @@ Node* create_node(unsigned int vertex) {
     return new;
 }
 
-Graph* create_graph(unsigned int B) {
+Graph* create_graph(unsigned int num_vertices) {
     Graph *new;
     new = (Graph*)malloc(sizeof(Graph));
-    new -> num_vertices = B;
-    new -> vertices = malloc((B + 1) * sizeof(VertexNode*));
-    new -> adjlist = malloc((B + 1) * sizeof(Node*));
-    new -> adjlist_rev = malloc((B + 1) * sizeof(Node*));
+    new -> num_vertices = num_vertices;
+    new -> vertices = malloc((num_vertices + 1) * sizeof(VertexNode*));
+    new -> adjlist = malloc((num_vertices + 1) * sizeof(Node*));
+    new -> adjlist_rev = malloc((num_vertices + 1) * sizeof(Node*));
 
-    for (unsigned int i=1; i<=B; i++) {
+    for (unsigned int i=1; i<=num_vertices; i++) {
         new -> vertices[i] = create_vertex_node();
         new -> adjlist[i] = NULL;
         new -> adjlist_rev[i] = NULL;
+    }
+
+    return new;
+}
+
+Graph* create_condense_graph(unsigned int num_vertices) {
+    Graph *new;
+    new = (Graph*)malloc(sizeof(Graph));
+    new -> num_vertices = num_vertices;
+    new -> vertices = malloc((num_vertices + 1) * sizeof(VertexNode*));
+    new -> adjlist = malloc((num_vertices + 1) * sizeof(Node*));
+
+    for (unsigned int i=1; i<=num_vertices; i++) {
+        new -> vertices[i] = create_vertex_node();
+        new -> adjlist[i] = NULL;
     }
 
     return new;
@@ -81,10 +96,16 @@ void print_graph(Graph* graph) {
     }
 }
 
-void dfs_visit(Graph* graph, unsigned int u, Node** finish_time) {
+void dfs_visit(Graph* graph, unsigned int u, Node** finish_time, bool* early_stop) {
+    if (early_stop != NULL && *early_stop == true) {
+        return;
+    }
+
     static unsigned int time = 0;
-    Node* tmp;
     unsigned int v;
+    bool exist = false;
+    Node* tmp;
+
     time = time + 1;
     graph->vertices[u]->discover = time;
     graph->vertices[u]->color = 'g';
@@ -92,30 +113,48 @@ void dfs_visit(Graph* graph, unsigned int u, Node** finish_time) {
     while (tmp != NULL) {
         v = tmp->vertex;
         if (graph->vertices[v]->color == 'w') {
-            dfs_visit(graph, v, finish_time);
+            dfs_visit(graph, v, finish_time, early_stop);
         }
         tmp = tmp->next;
     }
     graph->vertices[u]->color = 'b';
     time = time + 1;
     graph->vertices[u]->finish = time;
-    if (finish_time == NULL) {
+    if (*finish_time == NULL) {
         *finish_time = create_node(u);
     } else {
+        v = (*finish_time)->vertex;
         tmp = create_node(u);
         tmp->next = *finish_time;
         *finish_time = tmp;
+
+        if (early_stop != NULL) {
+            tmp = graph->adjlist[u];
+            while (tmp!=NULL) {
+                if (tmp->vertex == v) {
+                    exist = true;
+                    break;
+                }
+                tmp = tmp->next;
+            }
+            if (exist == false) {
+                *early_stop = true;
+            }
+        }
     }
 }
 
-void dfs(Graph* graph, Node** finish_time) {
+void dfs(Graph* graph, Node** finish_time, bool* early_stop) {
     unsigned int u;
     for (u=1; u<=graph->num_vertices; u++) {
         graph->vertices[u]->color == 'w';
     }
     for (u=1; u<=graph->num_vertices; u++) {
         if (graph->vertices[u]->color == 'w') {
-            dfs_visit(graph, u, finish_time);
+            dfs_visit(graph, u, finish_time, early_stop);
+            if (early_stop != NULL && *early_stop == true) {
+                break;
+            }
         }
     }
 }
@@ -169,12 +208,18 @@ void dfs_transpose(Graph* graph, Node* finish_time, unsigned int* group) {
     }
 }
 
+// void topological_sort(Graph* condense_graph,)
 int main() {
     unsigned int B, A, mode;
     unsigned int u, v;
-    Node* new = NULL;
-    Node* finish_time = NULL;
+    unsigned int x, y;
     unsigned int group = 0;
+    bool topological_sort_early_stop = false;
+    Node* new = NULL;
+    Node* tmp = NULL;
+    Node* finish_time = NULL;
+    Node* topological_sort_finish_time = NULL;
+
     scanf("%u%u%u", &B, &A, &mode);
 
     Graph* graph = create_graph(B);
@@ -191,15 +236,43 @@ int main() {
         graph->adjlist_rev[v] = new;
     }
     
-    dfs(graph, &finish_time);
+    dfs(graph, &finish_time, NULL);
     dfs_transpose(graph, finish_time, &group);
+    printf("%u\n", group);
 
     // print_graph(graph);
     // print_finish_time(finish_time);
-    printf("%u\n", group);
-    if (mode==2) {
-        printf("%u\n", 0);
-    }
+    // printf("\n");
 
+    if (mode==2) {
+        Graph* condense_graph = create_condense_graph(group);
+        while (finish_time!=NULL) {
+            u = finish_time->vertex;
+            x = graph->vertices[u]->group;
+
+            tmp = graph->adjlist[u];
+            while (tmp != NULL) {
+                v = tmp->vertex;
+                y = graph->vertices[v]->group;
+                if (x!=y) {
+                    new = create_node(y);
+                    new->next = condense_graph->adjlist[x];
+                    condense_graph->adjlist[x] = new;
+                }
+                tmp = tmp->next;
+            }
+            finish_time = finish_time->next;
+        }
+
+        dfs(condense_graph, &topological_sort_finish_time, &topological_sort_early_stop);
+        if (topological_sort_early_stop) {
+            printf("0\n");
+        } else {
+            printf("1\n");
+        }
+        // print_graph(condense_graph);
+        // print_finish_time(topological_sort_finish_time);
+        // printf("\n");
+    }
     return 0;
 }
