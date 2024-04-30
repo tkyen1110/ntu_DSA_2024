e09188d050387d898ec99ce0696174a7f1747a01
tkyen <tkyen1110@gmail.com>
2024-04-30 09:50:09 +0800

    Finish HW3/Problem4.c

--- HW3/Problem4.c
+++ HW3/Problem4.c
@@ -8,131 +8,69 @@
 
 // https://www.youtube.com/watch?v=af1oqpnH1vA
 // https://www.naukri.com/code360/library/prefix-function-knuth-morris-pratt-algorithm
-void compute_prefix_function(char* P, unsigned int** pi) {
+void compute_prefix_function(char* P, unsigned int* pi) {
     // P : pattern
+    // m : pattern length
     // pi: prefix function
     unsigned int m = strlen(P); // pattern length
     unsigned int i, j;
-    *pi = (unsigned int*)malloc(sizeof(unsigned int)*m);
-    (*pi)[0] = 0;
+
+    pi[0] = 0;
 
     for (i=1; i<m; i++) {
-        j = (*pi)[i-1];
+        j = pi[i-1];
         while (j>0 && P[i]!=P[j]) {
-            j = (*pi)[j-1];
+            j = pi[j-1];
         }
 
         if (P[i]==P[j]) {
             j = j + 1;
         }
-        (*pi)[i] = j;
+        pi[i] = j;
     }
 
     // printf("prefix function: ");
     // for (i=0; i<m; i++) {
-    //     printf("%u ", (*pi)[i]);
+    //     printf("%u ", pi[i]);
     // }
     // printf("\n");
 }
 
-unsigned int kmp_matcher(char* T, char* P, unsigned int* pi, unsigned int m) {
+unsigned int kmp_matcher(char* T, char* P, unsigned int* pi, unsigned int* hit_count, unsigned int* plagiarism_likelihood) {
     unsigned int n = strlen(T); // text length
-    // unsigned int m = strlen(P); // pattern length
+    unsigned int m = strlen(P); // pattern length
     unsigned int i, j = 0;
-    unsigned int hit_count = 0;
-    if (n < m) {
-        return hit_count;
-    }
+
     for (i=0; i<n; i++) {
         while (j>0 && T[i]!=P[j]) {
             j = pi[j-1];
         }
         if (T[i]==P[j]) {
+            hit_count[j] = hit_count[j] + 1;
             j = j + 1;
         }
         if (j==m) {
             // printf("Pattern occurs with shift %u\n", i+1-m);
-            hit_count = hit_count + 1;
             j = pi[j-1];
         }
     }
-    return hit_count;
-}
 
-// https://www.naukri.com/code360/library/count-all-substrings-of-all-lengths-that-match-exactly-with-the-prefix-and-suffix-of-a-string
-// https://www.naukri.com/code360/guided-paths/interview-guide-for-product-based-companies/content/111307/offering/1376457
-// https://www.naukri.com/code360/library/length-of-all-prefixes-that-are-also-the-suffixes-of-the-given-string
-// https://codeforces.com/blog/entry/3107?#comment-692814
+    for (i=0; i<m; i++) {
+        j = m-1-i;
+        if (pi[j]>0) {
+            hit_count[pi[j]-1] += hit_count[j];
+        }
 
-
-// https://hackmd.io/@Cutiemango/B1A8muQIL
-unsigned int compute_z_function(char* P, unsigned int** z, unsigned int m) {
-    // unsigned int m = strlen(P);
-    unsigned int i, L=0, R=0;
-    unsigned int plagiarism_likelihood = 1;
-
-    *z = (unsigned int*)malloc(sizeof(unsigned int)*m);
-    (*z)[0] = 0;
-
-    for (i = 1; i < m; i++) {
-        if (i + (*z)[i-L] < R) {
-            (*z)[i] = min((*z)[i-L], R-i);
+        if (pi[i]>0) {
+            plagiarism_likelihood[i] = plagiarism_likelihood[pi[i]-1] + 1;
         } else {
-            while (i + (*z)[i] < m && P[(*z)[i]] == P[i + (*z)[i]]) {
-                (*z)[i]++;
-            }
-        }
-
-        if (m-i == (*z)[i]) {
-            plagiarism_likelihood++;
-        }
-    
-        if (i + (*z)[i] > R) {
-            R = i + (*z)[i];
-            L = i;
+            plagiarism_likelihood[i] = 1;
         }
     }
 
-    // printf("z: ");
-    // for (i = 0; i < m; i++) {
-    //     printf("%u ", (*z)[i]);
+    // for (i=0; i<m; i++) {
+    //     printf("hit_count = %u / plagiarism_likelihood = %u\n", hit_count[i], plagiarism_likelihood[i]);
     // }
-    // printf("\n");
-    return plagiarism_likelihood;
-}
-
-// https://codeforces.com/blog/entry/9612?locale=ru#comment-217621
-unsigned int prefix_function_to_z_function(char* P, unsigned int* pi, unsigned int** z, unsigned int m) {
-    // unsigned int m = strlen(P);
-    unsigned int i, j, v;
-    unsigned int plagiarism_likelihood = 1;
-
-    *z = (unsigned int*)malloc(sizeof(unsigned int)*m);
-    (*z)[0] = 0;
-
-    for (i = 1; i < m; i++) {
-        (*z)[i] = 0;
-        if (pi[i]) {
-            (*z)[i - pi[i] + 1] = pi[i];
-        }
-    }
-
-    for (i = 1; i < m; ) {
-        for (j = 1; j < (*z)[i] && (v = min((*z)[j], (*z)[i] - j)) >= (*z)[i + j] ; ++j) {
-            (*z)[i + j] = v;
-        }
-        i += j;
-    }
-
-    // printf("z: ");
-    for (i = 0; i < m; i++) {
-        if (m-i == (*z)[i]) {
-            plagiarism_likelihood++;
-        }
-        // printf("%u ", (*z)[i]);
-    }
-    // printf("\n");
-    return plagiarism_likelihood;
 }
 
 int main() {
@@ -148,16 +86,19 @@ int main() {
     }
     // printf("Text    : %s %lu\n", T, strlen(T));
     // printf("Pattern : %s %lu\n", P, strlen(P));
+    unsigned int m = strlen(P);
+    unsigned int* pi = (unsigned int*)malloc(sizeof(unsigned int)*m);
+    unsigned int* hit_count = (unsigned int*)malloc(sizeof(unsigned int)*m);
+    unsigned int* plagiarism_likelihood = (unsigned int*)malloc(sizeof(unsigned int)*m);
+    memset(pi, 0, sizeof(unsigned int)*m);
+    memset(hit_count, 0, sizeof(unsigned int)*m);
+    memset(plagiarism_likelihood, 0, sizeof(unsigned int)*m);
 
-    unsigned int* pi;
-    unsigned int *z1, *z2;
-    compute_prefix_function(P, &pi);
-    for (unsigned int m = 1; m <= strlen(P); m++) {
-        printf("%llu\n", (unsigned long long)kmp_matcher(T, P, pi, m)*compute_z_function(P, &z1, m));
-        // printf("%u\n", kmp_matcher(T, P, pi, m));
-        // printf("%u\n", compute_z_function(P, &z1, m));
-        // printf("%u\n", prefix_function_to_z_function(P, pi, &z2, m));
-        // printf("\n");
+    compute_prefix_function(P, pi);
+    kmp_matcher(T, P, pi, hit_count, plagiarism_likelihood);
+
+    for (unsigned int i = 0; i < m; i++) {
+        printf("%llu\n", (unsigned long long)hit_count[i]*plagiarism_likelihood[i]);
     }
     return 0;
 }
